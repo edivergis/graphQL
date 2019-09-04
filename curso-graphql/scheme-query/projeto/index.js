60bbd194015b3d26ea8ce3b2c421d2008f291444
edizarp <edi.vergis@zarpsystem.com.br>
2019-09-04 00:37:38 -0300

    Criando Types e Resolvers

--- curso-graphql/scheme-query/projeto/index.js
+++ curso-graphql/scheme-query/projeto/index.js
@@ -1,2 +1,84 @@
+const { ApolloServer, gql } = require('apollo-server')
 
-console.log("teste")
+const typeDefs = gql`
+
+    scalar Date
+
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    #pontos de entrada da sua API!
+    type Query {
+        ola: String
+        horaAtual: Date
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+    }
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto:Float
+    }
+`
+
+const resolvers = {
+    Produto: {
+        precoComDesconto(produto){
+            if (produto.desconto) {
+                return produto.preco - (produto.preco * produto.desconto)
+            }
+            return produto.preco
+            
+        }
+    },
+    Usuario:{
+        salario(usuario) {
+            return usuario.salario_atual
+        }
+    },
+    Query: {
+        produtoEmDestaque(){
+            return {
+                nome: 'macBook Pro',
+                preco: '10000.00',
+                desconto: 0.15,
+            }
+        },
+        ola() {
+            return 'Its Work!'
+        },
+        horaAtual() {
+            return new Date
+        },
+        usuarioLogado() {
+            return {
+                id: 111,
+                nome: 'evergis',
+                email: 'edi.vergis@gmail.com',
+                idade: 35,
+                salario_atual: 1234.56,
+                vip: true
+            }
+        }
+    }
+
+}
+
+const server = new ApolloServer({
+
+    typeDefs,
+    resolvers
+
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
