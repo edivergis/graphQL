e6d2527d091b98c89e64bbdf26c940aa75de2748
edizarp <edi.vergis@zarpsystem.com.br>
2019-09-12 09:23:39 -0300

    Mutation de exclusão de usuário

--- curso-graphql/scheme-query/projeto/resolvers/Mutation.js
+++ curso-graphql/scheme-query/projeto/resolvers/Mutation.js
@@ -16,5 +16,29 @@ module.exports = {
 
        usuarios.push(novo)
        return novo
+   }, 
+   excluirUsuario(_, { id }) {
+       const i = usuarios.findIndex(u => u.id === id)
+       if (i < 0) return null
+       const excluidos = usuarios.splice(i, 1)
+       return excluidos ? excluidos[0] : null
+   },
+   alterarUsuario(_, args) {
+       const i = usuarios.findIndex(u => u.id === args.id)
+       if (i < 0) return null
+
+       usuarios[i].nome = args.nome
+       usuarios[i].email = args.email
+       usuarios[i].idade = args.idade
+
+    //    const usuario = {
+    //        //mantem o último array de argumentos, caso haja conflitantes
+    //        ...usuarios[i],
+    //        ...args
+    //    }
+
+    //    usuarios.splice(i, 1, usuario)
+    //    return usuario
+    return usuarios[i]
    }
 }
