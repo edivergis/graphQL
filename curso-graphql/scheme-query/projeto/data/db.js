f1e939c20918d63c7a42cd2170f742c26be0ceb2
edizarp <edi.vergis@zarpsystem.com.br>
2019-09-11 22:51:45 -0300

    Enums

--- curso-graphql/scheme-query/projeto/data/db.js
+++ curso-graphql/scheme-query/projeto/data/db.js
@@ -3,25 +3,29 @@ const usuarios = [{
     nome: 'Edi Vergis',
     email: 'evergis@zemail.com',
     idade: 35,
-    perfil_id:1
+    perfil_id:1,
+    status: 'ATIVO'
 },{
     id: 2,
     nome: 'Bruna Vergis',
     email: 'bvergis@zemail.com',
     idade: 27,
-    perfil_id:1
+    perfil_id:1,
+    status: 'ATIVO'
 },{
     id: 3,
     nome: 'Daniel Vergis',
     email: 'dvergis@zemail.com',
     idade: 3,
-    perfil_id:2
+    perfil_id:2,
+    status: 'INATIVO'
 },{
     id: 4,
     nome: 'Calebe Vergis',
     email: 'cvergis@zemail.com',
     idade: 2,
-    perfil_id:2
+    perfil_id:2,
+    status: 'BLOQUEADO'
 }]
 
 const perfis = [{
